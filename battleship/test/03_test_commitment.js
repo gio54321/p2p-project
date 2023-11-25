b8718ea8bacf68a27a7cb7a539435f898fa1a3df
gio54321 <g.dellimmagine@gmail.com>
2023-11-25 17:38:39 +0100

    Minor cleaning test

--- battleship/test/03_test_commitment.js
+++ battleship/test/03_test_commitment.js
@@ -10,19 +10,13 @@ contract("Battleship", (accounts) => {
         const battleship = await Battleship.deployed();
         const board = [1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1];
         const boardValues = board.map(utils.generateBoardValue);
-        // console.log(boardValues);
         const commitments = boardValues.map(utils.generateCommitment);
-        // console.log(commitments);
 
         const root = utils.computeMerkleRoot(commitments);
-        // console.log('root', root);
 
         for (let i=0; i<board.length; ++i) {
             const proof = utils.computeMerkleProof(commitments, i);
-            // console.log(proof);
-
             const rootComputedByContract = await battleship.computeMerkleRootFromProof.call(board.length, i, boardValues[i], proof);
-            // console.log('result from contract', rootComputedByContract);
             assert.equal(root, rootComputedByContract);
         }
 
