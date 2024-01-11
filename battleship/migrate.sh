606c3a25d17c29d499022bbf31580913fb377981
gio54321 <g.dellimmagine@gmail.com>
2024-01-11 15:30:50 +0100

    Update migration script

--- battleship/migrate.sh
+++ battleship/migrate.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-truffle migrate && cp build/contracts/Battleship.json ../frontend/src/lib/contracts/Battleship.json
+mkdir -p ../frontend/src/lib/contracts &&\
+    truffle migrate &&\
+    cp build/contracts/Battleship.json ../frontend/src/lib/contracts/Battleship.json
